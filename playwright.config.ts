f820bcd0b449a633226f09129a23a671744cefbf
EvanSmith93 <evan.smith993@gmail.com>
2025-04-01 23:27:00 -0600

    revert: playwright testing url

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   reporter: "html",
   timeout: 5000,
   use: {
-    baseURL: "http://localhost:5175",
+    baseURL: "http://localhost:5173",
     trace: "on-first-retry",
   },
 
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:5175",
+    url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
     timeout: 5000,
   },
